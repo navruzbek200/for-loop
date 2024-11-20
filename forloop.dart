d7074654b6ff2949b15d8db8eb00343238d7aea8
navruzbek200 <navrozbekbektemirov7@gmail.com>
2024-11-20 09:41:04 +0500

    for loop practice

---
+++ forloop.dart
@@ -0,0 +1,9 @@
+void main(){
+
+  int number = 5;
+  for(int count = 0; count <= number; count++){
+print(count);
+
+
+  }
+}
---
+++ forloop1+25.dart
@@ -0,0 +1,18 @@
+void main(){
+  int s = 0;
+  // int count = 1;
+
+
+for (int count = 1; count <=25; count++){
+
+  print("count: $count");
+
+  s= s + count; // s = 0 + 1 + 2 + 3 ... + 25
+
+
+
+}
+print("Natija: $s");
+
+
+}
